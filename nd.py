ab80e224f103518316ba55d7abd0d3596941d49c
- <>
2025-05-03 17:54:14 +0530

    -

--- nd.py
+++ nd.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 import sys
+import curses
+import random
 nds=open('nds','r').read().split('\n')
-
+s=curses.initscr()
+k=-1
+if len(sys.argv)>1:
+    k=int(sys.argv[1])
+while 1:
+    p=s.getch()
+    if p=='n':break
+    if k==-1:
+        l=nds[random.randint(0,len(nds))]
+    else:
+        if random.randint(0,2):
+            l=nds[k]
+        else: l=nds[random.randint(0,len(nds)-1)]
+curses.endwin()
